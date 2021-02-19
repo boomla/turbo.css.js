6a3c340b51d45cd6303af97dd5fe84626080b6ef
zupa-hu <thalter@zupa.hu>
2021-02-19 21:06:37 +0100

    reimport specification files (rename)

--- src/spec/PARSE_ALL_test.ts
+++ src/spec/PARSE_ALL_test.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 
 describe('parse all spec documents', function() {
 	it('should successfully parse all specification docs', function() {
-		let paths = glob.sync("./src/spec/v1/**/*.bat");
+		let paths = glob.sync("./src/spec/v1/**/*.txt");
 
 		if (paths.length < 100 || (300 < paths.length)) {
 			throw new Error("too few specification documents found ["+paths.length+"]");
